9749a6020e241b2769f506d775460fdff43eb706
Your Name <your@email.com>
2026-03-10 17:04:55 +0900

    etc

---
+++ app/robots.ts
@@ -0,0 +1,22 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: ['/'],
+        disallow: [
+          '/api/',
+          '/auth/',
+          '/dashboard/',
+          '/admin/',
+          '/agency/',
+          '/hospital/',
+        ],
+      },
+    ],
+    sitemap: 'https://www.relynplatform.com/sitemap.xml',
+    host: 'https://www.relynplatform.com',
+  };
+}
